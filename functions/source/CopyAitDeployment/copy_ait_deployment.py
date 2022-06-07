d0dfcea6b4bc7b01722f2303bba267c789c73b63
Vivek Katial <vivekkatial@gmail.com>
2022-06-07 11:21:20 -0700

    adding efs stuff

--- functions/source/CopyAitDeployment/copy_ait_deployment.py
+++ functions/source/CopyAitDeployment/copy_ait_deployment.py
@@ -1,26 +1,43 @@
-import cfnresponse
-import json
+import os
+import shutil
+import subprocess
+import pathlib
+import urllib.request
+import zipfile
+import tarfile
+from crhelper import CfnResource
+
+helper = CfnResource()
+
+@helper.create
+@helper.update
+def clone_deployment(event, _):
+    if os.listdir("/tmp"):
+        folder = '/tmp'
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+    url = 'https://github.com/NASA-AMMOS/AIT-Core/archive/refs/tags/2.3.5.tar.gz'
+    filehandle = urllib.request.urlopen(url)
+    tar = tarfile.open(fileobj=filehandle, mode="r|gz")
+    tar.extractall("/tmp")
+    tar.close()
+
+    pathlib.Path("/mnt/efs/ait").mkdir(parents=True, exist_ok=True)
+
+    shutil.copytree("/tmp/ait", "/mnt/efs/ait", dirs_exist_ok=True)
+
+    helper.Data['message'] = str(os.listdir("/mnt/efs/"))
+@helper.delete
+def no_op(_, __):
+    pass
 
 def handler(event, context):
-    """Lambda Handler for dealing with creating a new cognito user using AWS Cognito
-
-    Args:
-        event (dict): Event dictionary from custom resource
-        context (obj): Context manager
-    """
-
-    print(json.dumps(event, default=str))
-
-    responseData = {}
-    status = cfnresponse.FAILED
-    if event["RequestType"] == "Create":
-        responseData["LambaTest"] = "Create"
-        status = cfnresponse.SUCCESS
-    elif event["RequestType"] in ["Delete", "Update"]:
-        # No action needs to be taken for delete or update events
-        status = cfnresponse.SUCCESS
-        responseData["LambaTest"] = "Not Create"
-    else:
-        responseData = {"Message": "Invalid Request Type"}
-
-    cfnresponse.send(event, context, status, responseData)
+    helper(event, context)
