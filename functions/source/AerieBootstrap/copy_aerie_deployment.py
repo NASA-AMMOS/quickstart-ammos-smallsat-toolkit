dccdff9e8d5717d40b4937747cab854923a9f35e
Shah <bhavin.t.shah@jpl.nasa.gov>
2022-08-22 08:17:42 -0700

    Remove dist info folder

--- functions/source/AerieBootstrap/copy_aerie_deployment.py
+++ functions/source/AerieBootstrap/copy_aerie_deployment.py
@@ -43,6 +43,7 @@ def clone_deployment(event, _):
     shutil.copytree("/tmp/deployment/hasura/metadata", "/mnt/efs/init_hasura", dirs_exist_ok=True)
 
     helper.Data['message'] = str(os.listdir("/mnt/efs/"))
+
 @helper.delete
 def no_op(_, __):
     pass
